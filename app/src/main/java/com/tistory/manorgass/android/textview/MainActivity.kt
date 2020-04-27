4781a66692eddb3dd6735e74bf8ed2cc8f160457
JungWon Kim <manorgass@gmail.com>
2020-04-27 22:47:09 +0900

    first commit

---
+++ app/src/main/java/com/tistory/manorgass/android/textview/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.tistory.manorgass.android.textview
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val watcher = object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                textView.text = p0.toString()
+            }
+        }
+        editText.addTextChangedListener(watcher)
+    }
+}
